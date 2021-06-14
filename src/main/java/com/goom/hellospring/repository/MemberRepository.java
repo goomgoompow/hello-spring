9fee55ddbc6978c6ee6d2ddd2f0018fec765ce4e
unierp <unierp@bizentro.com>
2021-06-14 13:06:21 +0900

    Domain, Repository 구현

---
+++ src/main/java/com/goom/hellospring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.goom.hellospring.repository;
+
+import com.goom.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
